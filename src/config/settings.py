1266d4cb05120a1d37d077c7a8b28ab1c4ffa9a3
glory <zamaraev@gmail.com>
2022-11-27 18:31:24 +0500

    add project structure

---
+++ src/config/settings.py
@@ -0,0 +1,34 @@
+import databases
+import sqlalchemy
+
+from time import strftime   # Load just the strftime Module from Time
+
+FILE_LOG_NAME = 'fastapi-nsi'
+FILE_LOG = str(strftime("%Y-%m-%d-%H-%M-%S") + '_' + FILE_LOG_NAME + '.log')
+FILE_LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
+
+FILE_FIELDS = 'fields.geojson'
+
+# FOLDER_IN  = 'C:\\TEMP\\Geodex_files'
+FOLDER_OUT = 'log'
+FOLDER_DATA = 'data'
+
+
+DB_SQLITE = "sqlite:///fastapi-nsi.db"
+# DB_SCHEMA = 'public'
+# DB_HOST = 'localhost'
+# DB_PORT = '5432'
+# DB_USER = 'geodex2'
+# DB_PASSWORD = 'geodex2_password'
+# DB_NAME = 'geodex2'
+# DB_SCHEMA = 'geodex2'
+# postgresql://udatauser2:udatauser2pwd@localhost:5432/udatadb2
+# DB_DSN = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+
+engine = sqlalchemy.create_engine(DB_SQLITE)
+
+metadata = sqlalchemy.MetaData()
+database = databases.Database(DB_SQLITE)
+
+
--- src/main.py
+++ src/main.py
@@ -1,91 +1,38 @@
 # uvicorn main:app --reload
+import uvicorn
+from fastapi import FastAPI
+from src.config.settings import database, metadata, engine
 
-from fastapi import FastAPI, Query, HTTPException, Path
-from pydantic import BaseModel
-from typing import Optional
-import json
-
+# , Query, HTTPException, Path
+# from typing import Union
+#
+# from pydantic import BaseModel
+# from typing import Optional
+# import json
 
 app = FastAPI()
 
-
-class Person(BaseModel):
-    id: Optional[int] = None
-    name: str
-    age: intgender: str
-
-
-with open('people.json','r') as f:
-    people = json.load(f)
+app.state.database = database
+@app.get("/")
+def read_root():
+    return {"msg": "Hello it is FastAPI-NSI project"}
 
 
-@app.get('/person/{p_id}', status_code = 200)
-def get_person(p_id: int):
-    person = [p for p in people if p['id'] == p_id]
-    return person[0]  if len(person) > 0 else {}
+@app.on_event("startup")
+async def startup() -> None:
+    database_ = app.state.database
+    metadata.create_all(engine)
+    if not database_.is_connected:
+        await database_.connect()
 
 
-@app.get('/search', status_code = 200)
-def search_person(age: Optional[int] = Query(None, title="Age", description = "The age to filter for"),
-                  name: Optional[str] = Query(None, title="Name", description = "The name to filter for")) :
-    people1 = [p for p in people if p['age'] == age]
-
-    if name is None:
-        if age is None:
-            return people
-        else: 
-            return person1         
-    else: 
-        people2 = [p for p in people if name.lower() in p['name'].lower() ]
-        if age is None:
-            return people2
-        else: 
-            combined = [p for p in people1 if p in people2]
-            return combined
-
-    
-@app.post('/addPerson', status_code = 201)
-def add_person(person: Person):
-    p_id = max([['id'] for p in people] + 1)
-    new_person = {
-        "id": p_id,
-        "name": person.name,
-        "age" : person.age,
-        "gender": person.gender
-
-    }
-    people.append(new_person)  
-    with open('people.json','w') as f:
-        people = json.dump(people, f)
-    return new_person
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    database_ = app.state.database
+    if database_.is_connected:
+        await database_.disconnect()
 
 
-@app.put( '/changePerson', status_code = 204)
-def change_person(person: Person)::
-    new_person = {
-        "id": person.id,
-        "name": person.name,
-        "age" : person.age,
-        "gender": person.gender
-    }
-    person_list = [p for p in people if p['id'] == person.id]
-    if len(person_list) > 0 :
-        people.remove(person_list[0])
-        people.append(new_person)
-        with open('people.json','w') as f:
-            people = json.dump(people, f)
-        return new_person
-    else:
-        raise HTTPException(status_code=404, detail = f"person with id {person.id} does not exist...")
-        
 
-@app.delete( '/deletePerson/{p_id}'', status_code = 204)
-def delete_person(p_id: int):
-    person = [p for p in people if p['id'] == p_id]
-
-    if len(person) > 0 :
-        people.remove(person[0])
-         with open('people.json','w') as f:
-            people = json.dump(people, f)
-     else:
-        raise HTTPException(status_code=404, detail = f"There is no person with id {p_id} .")
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
